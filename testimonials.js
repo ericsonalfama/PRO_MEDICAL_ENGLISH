4aea2c578026ed6314cbc8e22260dddb4811eb9d
ericsonalfama <102916183+ericsonalfama@users.noreply.github.com>
2024-09-27 23:30:14 +0200

    Update testimonials.js

--- testimonials.js
+++ testimonials.js
@@ -1,32 +1,26 @@
-// Selecionando os elementos necessários
 const testimonials = document.querySelectorAll('.testimonial-block');
 const nextButton = document.querySelector('.arrow-right');
 const prevButton = document.querySelector('.arrow-left');
 
-let currentIndex = 0; // Índice do depoimento atual
+let currentIndex = 0;
 
-// Função para atualizar os depoimentos
 function updateTestimonials() {
     testimonials.forEach((testimonial, index) => {
         testimonial.style.display = index === currentIndex ? 'block' : 'none';
     });
 }
 
-// Função para ir para o próximo depoimento
 function nextTestimonial() {
-    currentIndex = (currentIndex + 1) % testimonials.length; // Aumenta o índice e reinicia se necessário
+    currentIndex = (currentIndex + 1) % testimonials.length;
     updateTestimonials();
 }
 
-// Função para ir para o depoimento anterior
 function prevTestimonial() {
-    currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length; // Diminui o índice e reinicia se necessário
+    currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
     updateTestimonials();
 }
 
-// Adicionando eventos aos botões
 nextButton.addEventListener('click', nextTestimonial);
 prevButton.addEventListener('click', prevTestimonial);
 
-// Inicializando a exibição dos depoimentos
 updateTestimonials();
